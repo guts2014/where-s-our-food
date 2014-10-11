1a5dee6fb86530b0a2a86757556a6349997da562
Jordan Duncan <jordan@dogfi.sh>
2014-10-11 10:20:40 +0100

    moved working dir to scrollingElements, ch ch ch changes

--- wheres_our_sound/script.js
+++ wheres_our_sound/script.js
@@ -1,14 +1,14 @@
 var timesPlayed = 0;
 
 //array that stores the notes
-noteArray = [4, 4, 3, 0, 2, 2, 1, 8]
+noteArray = [4, 4, 3, 0, 2, 2, 1, 8];
 
 function playArray() {
 	for(i=0; i<8; i++){
 		(function(i){
 			setTimeout(function(){
-				playNote(noteArray[i])
-			}, 500 * i);
+				playNote(noteArray[i]);
+			}, 250 * i);
 		}(i));
 	}
 }
