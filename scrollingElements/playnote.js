d8d498dfb92b64a4fe17b548eef0280b93927824
MariusVin <m.vinickis@gmail.com>
2014-10-12 13:59:45 +0100

    db1

--- scrollingElements/playnote.js
+++ scrollingElements/playnote.js
@@ -1,1 +1,24 @@
 //play single note
+<<<<<<< Updated upstream
+=======
+
+var audio = [
+	new Audio('sounds/horn_c_penta/0.wav'),
+	new Audio('sounds/horn_c_penta/1.wav'),
+	new Audio('sounds/horn_c_penta/2.wav'),
+	new Audio('sounds/horn_c_penta/3.wav'),
+	new Audio('sounds/horn_c_penta/4.wav'),
+	new Audio('sounds/horn_c_penta/5.wav'),
+	new Audio('sounds/horn_c_penta/6.wav'),
+	new Audio('sounds/horn_c_penta/7.wav'),
+	new Audio('sounds/horn_c_penta/8.wav')
+];
+function playNote(note){
+
+	if (note!=0) {
+		audio[note].play();
+	}
+	timesPlayed += 1;
+	
+}
+>>>>>>> Stashed changes
